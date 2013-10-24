afdbee539205367d3151884cd101c6c6f49dfc0b
Ole Bakman Borup <obo@dma.dk>
2013-10-24 12:20:37 +0200

    Checkstyle changes

--- ais-virtualnet-server/src/main/java/dk/dma/ais/virtualnet/server/AisVirtualNetServer.java
+++ ais-virtualnet-server/src/main/java/dk/dma/ais/virtualnet/server/AisVirtualNetServer.java
@@ -9,7 +9,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -146,7 +146,8 @@ public class AisVirtualNetServer extends Thread implements Consumer<AisPacket> {
      */
     public void addClient(WebSocketServerSession session) {
         LOG.info("Adding client");
-        clients.add(session);
+        clients.add(session);        
+        LOG.info("Client count: " + clients.size());
     }
     
     /**
